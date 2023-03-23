0795261e47193ca2d5dcb25336929ea15a9f35ab
Elankumaran98 <siva98kumarane@gmail.com>
2023-03-23 09:13:31 +0530

    home page re create

--- client/src/page/Home.js
+++ client/src/page/Home.js
@@ -1,31 +1,36 @@
-import React, { useState, useEffect } from 'react';
-import { onAuthStateChanged } from "firebase/auth";
-import { auth } from '../firebase';
+import React from 'react';
+import {  signOut } from "firebase/auth";
+import {auth} from '../../firebase';
+import { useNavigate } from 'react-router-dom';
  
 const Home = () => {
+    const navigate = useNavigate();
  
-    useEffect(()=>{
-        onAuthStateChanged(auth, (user) => {
-            if (user) {
-              // User is signed in, see docs for a list of available properties
-              // https://firebase.google.com/docs/reference/js/firebase.User
-              const uid = user.uid;
-              // ...
-              console.log("uid", uid)
-            } else {
-              // User is signed out
-              // ...
-              console.log("user is logged out")
-            }
-          });
-         
-    }, [])
+    const handleLogout = () => {               
+        signOut(auth).then(() => {
+        // Sign-out successful.
+            navigate("/");
+            console.log("Signed out successfully")
+        }).catch((error) => {
+        // An error happened.
+        });
+    }
+   
+    return(
+        <>
+            <nav>
+                <p>
+                    Welcome Home
+                </p>
  
-  return (
-    <section>        
-      …
-    </section>
-  )
+                <div>
+        			<button onClick={handleLogout}>
+                        Logout
+                    </button>
+        		</div>
+            </nav>
+        </>
+    )
 }
  
-export default Home
+export default Home;
