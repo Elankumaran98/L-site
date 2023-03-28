82c79586f395192e0f8304f855e21c23f7aeed81
Elankumaran98 <siva98kumarane@gmail.com>
2023-03-28 11:20:32 +0530

    firestore added

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,5 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyBoanwj9OYnT45hd-5QsNPw1hKLIytrMgs",
@@ -14,4 +16,5 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth=getAuth(app)
+export const db=getFirestore(app);
 export default app;
