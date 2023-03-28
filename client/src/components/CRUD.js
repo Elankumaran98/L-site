b4c8ed62c0b659c9fa7dfb13d618138a33bcfd05
Elankumaran98 <siva98kumarane@gmail.com>
2023-03-28 13:59:31 +0530

    crud operation done

--- client/src/components/CRUD.js
+++ client/src/components/CRUD.js
@@ -1,9 +1,52 @@
-import React from 'react'
+import {
+  query,
+  collection,
+  limit,
+  onSnapshot,
+  doc,
+  updateDoc,
+  deleteDoc,
+} from "firebase/firestore";
+import React, { useState, useEffect } from "react";
+import { db } from "../firebase.js";
+export const userCollectionRef = collection(db, "users");
 
 const CRUD = () => {
-  return (
-    <div>CRUD</div>
-  )
-}
+  const [users, setUsers] = useState([]);
 
-export default CRUD
+  useEffect(() => {
+    const quaryResponse = query(userCollectionRef, limit(10));
+    const snapShot = onSnapshot(quaryResponse, (snapShotParam) => {
+      setUsers(
+        snapShotParam.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }))
+      );
+    });
+    return () => snapShot();
+  }, []);
+
+  const updateAge = async (userId, updateAge) => {
+    const userDoc = doc(db, "users", userId);
+    const newAge = { age: updateAge };
+    await updateDoc(userDoc, newAge);
+  };
+
+  const deleteUser = async (userId) => {
+    const userDoc = doc(db, "users", userId);
+    await deleteDoc(userDoc);
+  };
+
+  return <div>
+    <ul>
+        {users.map((user)=>{
+            return (
+                <li key={user.id}>Name :{user.name} {user.age}</li>
+            )
+        })}
+    </ul>
+  </div>;
+};
+
+export default CRUD;
--- client/src/components/CreateUser.js
+++ client/src/components/CreateUser.js
@@ -1,11 +1,57 @@
 import React,{useState} from 'react'
+import {addDoc} from 'firebase/firestore'
+import { userCollectionRef } from './CRUD';
 
-const CreateUser = () => {
-    const [name,setName]=useState();
-    const [age,setAge]=useState();
-  return (
-    <div>CreateUser</div>
-  )
-}
 
-export default CreateUser
+function CreateUser() {
+    const [name, setName] = useState();
+    const [age, setAge] = useState();
+  
+    const handleChange = (e) => {
+      if(e.target.name==='name'){
+          setName(e.target.value)
+      }else{
+          setAge(Number(e.target.value))
+      }
+    };
+  
+    const handleSubmit=async()=>{
+      await addDoc(userCollectionRef,{name,age})
+      setName('')
+      setAge('')
+    }
+    return (
+      <div>
+        <div className="mb-3">
+          <label htmlFor="name" className="form-label">
+            Name
+          </label>
+          <input
+            type="text"
+            name="name"
+            className="form-control"
+            id="name"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="age" className="form-label">
+            Age
+          </label>
+          <input
+            type="number"
+            name="age"
+            className="form-control"
+            id="age"
+            onChange={handleChange}
+          />
+        </div>
+        <button type="submit" className="btn btn-primary" onClick={handleSubmit}>
+          Add
+        </button>
+      </div>
+    );
+  }
+  
+  export default CreateUser;
+  
