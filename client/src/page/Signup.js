38b9722d1ef3a915714b45fd9d7002c4d846c07f
Elankumaran98 <siva98kumarane@gmail.com>
2023-04-04 10:03:12 +0530

    bug fixed

--- client/src/page/Signup.js
+++ client/src/page/Signup.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../firebase";
-import "../App.css"
+import "../App.css";
 
 
 const Signup = () => {
@@ -10,7 +10,7 @@ const Signup = () => {
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
+  
   const onSubmit = async (e) => {
     e.preventDefault();
 
@@ -30,49 +30,52 @@ const Signup = () => {
   return (
     <main className="container p-3 border border-danger rounded mt-2">
       <section>
-      <div>
-            <h1 className="text-center"> LShop </h1>
-            <form>
-              <div className="form-group">
-                <label htmlFor="email-address">Email address</label>
-                <input
+        <div>
+          <h1 className="text-center"> LShop </h1>
+          <form>
+            <div className="form-group">
+              <label htmlFor="email-address">Email address</label>
+              <input
                 className="form-control"
-                  type="email"
-                  label="Email address"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  required
-                  placeholder="Email address"
-                />
-              </div>
+                type="email"
+                label="Email address"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+                placeholder="Email address"
+              />
+            </div>
 
-              <div className="form-group">
-                <label htmlFor="password">Password</label>
-                <input
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <input
                 className="form-control"
-                  type="password"
-                  label="Create password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  required
-                  placeholder="Password"
-                />
-              </div>
-              <div className="form-group">
-                <button
-                  className="btn btn-outline-success"
-                  type="submit"
-                  onClick={onSubmit}
-                >
-                  Sign up
-                </button>
-              </div>
-            </form>
+                type="password"
+                label="Create password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+                placeholder="Password"
+              />
+            </div>
+            <div className="form-group">
+              <button
+                className="btn btn-outline-success"
+                type="submit"
+                onClick={onSubmit}
+              >
+                Sign up
+              </button>
+            </div>
+          </form>
 
-            <p>
-              Already have an account? <NavLink id="Nav" to="/login">Sign in</NavLink>
-            </p>
-          </div>
+          <p>
+            Already have an account?{" "}
+            <NavLink id="Nav" to="/login">
+              Sign in
+            </NavLink>
+          </p>
+        </div>
       </section>
     </main>
   );
